400cfb2c3430a3d90fe218ed6d202c26164363aa
vivien zhu <yuanzyy@gmail.com>
2021-03-02 22:25:50 -0500

    Case route Model Controller View seed complete

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -24,18 +24,30 @@ class CasesController < ApplicationController
   end
 
   def edit
-
+    @case = Case.find(params[:id])
   end
 
   def update
+    @case = Case.find(params[:id])
+
+    if @case.update(case_params)
+      redirect_to case_path(@case)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @case = Case.find(params[:id])
+    @case.destroy
+
+    redirect_to cases_path
   end
 
   private
 
   def case_params
+    # Nested Attributes: create patient and case at the same time
     params.require(:case).permit(:description,
                                  patient_attributes: [:first_name, :last_name, :gender,
                                                       :date_of_birth, :address])
