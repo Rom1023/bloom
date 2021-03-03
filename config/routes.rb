aaa8363d970ea631fd081c71d18ee9baaef7c906
vivien zhu <yuanzyy@gmail.com>
2021-03-03 13:39:25 -0500

    user account mcv complete. My profile show page complete

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  resources :users, only: [:show]
+
   resources :projects, only: [:index, :show, :new, :create, :edit, :update] do
     resources :links, only: [:create]
   end
