1acb686e0d57e8b99a87d0c61ad323be24ed1c7a
Marjorie Joseph <marjo_joseph86@hotmail.com>
2021-03-03 21:37:07 -0500

    added content in schema comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_012215) do
+ActiveRecord::Schema.define(version: 2021_03_04_022954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_03_04_012215) do
     t.bigint "patient_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
     t.index ["patient_id"], name: "index_cases_on_patient_id"
     t.index ["user_id"], name: "index_cases_on_user_id"
   end
@@ -38,6 +39,7 @@ ActiveRecord::Schema.define(version: 2021_03_04_012215) do
   create_table "comments", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "content"
   end
 
   create_table "links", force: :cascade do |t|
