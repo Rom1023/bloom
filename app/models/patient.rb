64454dff7a08d26ddfd2f03042221b0c38795f1a
vivien zhu <yuanzyy@gmail.com>
2021-03-03 22:36:09 -0500

    case index page adjusted

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,4 +3,8 @@ class Patient < ApplicationRecord
   has_many :users, through: :cases
 
   validates :first_name, :last_name, presence: true
+
+  def age
+    Date.today.year - self.date_of_birth.year
+  end
 end
