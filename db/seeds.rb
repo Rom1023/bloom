9a63dee718750a56c181910b09545a773b3a8f87
vivien zhu <yuanzyy@gmail.com>
2021-03-04 12:03:35 -0500

    building seed in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
 
 puts "destroying records..."
 
@@ -23,26 +24,25 @@ project_1 = Project.create!(name: "test one", description: "This is a test")
 project_2 = Project.create!(name: "test two", description: "Second test", completed: true)
 
 # Patient
-# patient_1 = Patient.create!(first_name: "John", last_name: "Test",
-#                             gender: "Male", date_of_birth: "1962-10-10",
-#                             address: "123 Fake st, Montreal")
-# patient_2 = Patient.create!(first_name: "May", last_name: "Test",
-#                             gender: "Female", date_of_birth: "1976-12-10",
-#                             address: "456 Fake st, Montreal")
+# patient_1 = Patient.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,
+#                             gender: Faker::Gender.binary_type, address: Faker::Address.street_address,
+#                             date_of_birth: Faker::Date.birthday(min_age: 10, max_age: 65))
 
 # Case + Patient
-case_1 = Case.new(description: "Leg bones, do not feel normal.",
-                  patient_attributes: {first_name: "John", last_name: "Test",
-                                        gender: "Male", date_of_birth: "1962-10-10",
-                                        address: "123 Fake st, Montreal"})
-case_1.user = user_1
-case_1.save!
+4.times do
+  case_p = Case.new(description: "Leg bones, do not feel normal.",
+                  patient_attributes: {first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,
+                                       gender: Faker::Gender.binary_type, address: Faker::Address.street_address,
+                                       date_of_birth: Faker::Date.birthday(min_age: 10, max_age: 65)})
+  case_p.user = user_1
+  case_p.save!
+end
 
-case_2 = Case.new(description: "Headache.",
-                  patient_attributes: {first_name: "May", last_name: "Test",
-                                        gender: "Female", date_of_birth: "1976-12-10",
-                                        address: "456 Fake st, Montreal"})
-case_2.user = user_2
-case_2.save!
+# case_2 = Case.new(description: "Headache.",
+#                   patient_attributes: {first_name: "May", last_name: "Test",
+#                                         gender: "Female", date_of_birth: "1976-12-10",
+#                                         address: "456 Fake st, Montreal"})
+# case_2.user = user_2
+# case_2.save!
 
 puts "done seeding..."
