0060ca1712dbd6bd43b687a24bd0e9ca585468ab
Marjorie Joseph <marjo_joseph86@hotmail.com>
2021-03-03 20:13:56 -0500

    added comments controller, view, router (show/create)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,4 +2,8 @@ class CommentsController < ApplicationController
   def index
     @comments = Comment.all
   end
+
+  def create
+    @comments = Comment.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :users, only: [:show]
-  resources :comments, only: [:show]
+  resources :comments, only: [:show, :create]
   resources :projects, only: [:index, :show, :new, :create, :edit, :update] do
     resources :links, only: [:create]
   end
