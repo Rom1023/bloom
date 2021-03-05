b6b3b8a6d592178c25cd1699c8578fc2d7402e01
elenicehalma <elenice.halma@gmail.com>
2021-03-05 13:44:53 -0500

    Work in progress.

--- app/models/project.rb
+++ app/models/project.rb
@@ -19,4 +19,9 @@ class Project < ApplicationRecord
     admin = collaborations.find_by(role: 'admin', user_id: user_id)
     admin.presence
   end
+
+  def my_collaborators
+     collaborations.collaborator
+  end
+
 end
